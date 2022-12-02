d12576e3f47c0cb38448a031ff14169bf1eeaa7e
WallaceRibeiro77 <wallaceribeiro72@gmail.com>
2022-12-02 17:36:57 -0300

    Projeto concluido

---
+++ src/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: black;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const Content = styled.div`
+    width: 60%;
+    background-color: #FFFFFF;
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
